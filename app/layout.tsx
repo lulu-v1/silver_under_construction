581645074680e4687d6d6035f88835916932992f
lulu-v2 <luca.izidi@gmail.com>
2025-11-19 14:05:55 +0100

    ajusting body width and scroll behaviour

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,35 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Silver Palace",
-  description: "Silver Palace Toulouse. Club de strip-tease et bar à hôtesses.",
+    title: "Silver Palace",
+    description: "Silver Palace Toulouse. Club de strip-tease et bar à hôtesses.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+            style={{margin: 0, padding: 0, overflow: "hidden", width: "100vw", height: "100vh"}}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
